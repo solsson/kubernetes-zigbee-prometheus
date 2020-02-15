a90ced3c59e1a7ba8307b85057184416f67569e1
Staffan Olsson <staffan@repos.se>
2020-02-15 17:36:07 +0100

    github.com/jurgen-kluft/go-conbee/blob/master/examples/sensors/get-all-sensors.go

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/solsson/kubernetes-zigbee-prometheus
+
+go 1.13
+
+require (
+	github.com/jurgen-kluft/go-conbee v0.0.0-20200204021822-a95f7fa490e5 // indirect
+	github.com/solsson/go-conbee v0.0.0-20200204021822-a95f7fa490e5
+)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/solsson/go-conbee/sensors"
+)
+
+var (
+	conbeeHost = "10.0.0.18"
+	conbeeKey  = "0A498B9909"
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: get-all-sensors -host=[string] -key=[string]\n")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func init() {
+	on := new(bool)
+	*on = true
+	flag.StringVar(&conbeeHost, "host", os.Getenv("DECONZ_CONBEE_HOST"), "conbee host addr")
+	flag.StringVar(&conbeeKey, "key", os.Getenv("DECONZ_CONBEE_APIKEY"), "conbee api key")
+	flag.Parse()
+	flag.Usage = usage
+}
+
+func main() {
+	if conbeeKey != "" {
+		ss := sensors.New(conbeeHost, conbeeKey)
+		sensors, err := ss.GetAllSensors()
+		if err != nil {
+			fmt.Println("sensors.GetAllSensors() ERROR: ", err)
+			os.Exit(1)
+		}
+		fmt.Println()
+		fmt.Println("Sensors")
+		fmt.Println("------")
+		for _, l := range sensors {
+			fmt.Printf("Sensor:\n%s\n", l.StringWithIndentation("  "))
+		}
+	} else {
+		usage()
+	}
+}
