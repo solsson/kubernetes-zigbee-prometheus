a40991d255a4d36fe6f8f8e363fca85b420ff391
Staffan Olsson <staffan@repos.se>
2020-02-16 11:48:39 +0100

    Adds logging

--- main.go
+++ main.go
@@ -6,19 +6,23 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/solsson/go-conbee/sensors"
-
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/solsson/go-conbee/sensors"
+
+	"go.uber.org/zap"
 )
 
 var (
 	conbeeHost = "10.0.0.18"
 	conbeeKey  = "0A498B9909"
+	metricsEndpoint string
+	metricsListen string
 )
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "usage: get-all-sensors -host=[string] -key=[string]\n")
+	fmt.Fprintf(os.Stderr, "usage: kubernetes-zigbee-prometheus -host=[string] -key=[string]\n")
 	flag.PrintDefaults()
 	os.Exit(2)
 }
@@ -28,20 +32,32 @@ func init() {
 	*on = true
 	flag.StringVar(&conbeeHost, "host", os.Getenv("DECONZ_CONBEE_HOST"), "conbee host addr")
 	flag.StringVar(&conbeeKey, "key", os.Getenv("DECONZ_CONBEE_APIKEY"), "conbee api key")
+	flag.StringVar(&metricsEndpoint, "endpoint", "/metrics", "Metrics endpoint path")
+	flag.StringVar(&metricsListen, "listen", ":8080", "Metrics http listen")
 	flag.Parse()
 	flag.Usage = usage
 }
 
 func main() {
+	logger, _ := zap.NewDevelopment()
+	defer logger.Sync()
 	if conbeeKey != "" {
-		fmt.Printf("Initializing sensors host %s key length %d", conbeeHost, len(conbeeKey))
+		logger.Info("failed to fetch URL",
+			zap.String("host", conbeeHost),
+			zap.Int("keylength", len(conbeeKey)),
+		)
+
 		ss = sensors.New(conbeeHost, conbeeKey)
 		foo := newDeconzCollector(ss)
 		prometheus.MustRegister(foo)
-	
-		http.Handle("/metrics", promhttp.Handler())
-		fmt.Println("Listening on port 8080")
-		http.ListenAndServe(":8080", nil)
+		logger.Info("Listening",
+			zap.String("endpoint", metricsEndpoint),
+			zap.String("on", metricsListen),
+		)
+
+		http.Handle(metricsEndpoint, promhttp.Handler())
+		
+		http.ListenAndServe(metricsListen, nil)
 	} else {
 		usage()
 	}
